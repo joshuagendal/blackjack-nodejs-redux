7b0b7d4434e077ebcaddc0748adfddc8df449b22
Joshua Gendal <joshgendal@gmail.com>
2019-01-09 02:22:14 -0700

    just pushing up an example for calling a promise within a function and calling async wait in a function

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-const { beginFauxGame } = require("./src/game/runner");
+// const { beginFauxGame } = require("./src/game/runner");
 
-beginFauxGame();
+// beginFauxGame();
+
+const promptly = require("promptly");
+
+// var x = () => {
+//   return new Promise((resolve, reject) => {
+//     promptly
+//       .prompt("Name")
+//       .then(nameResult => {
+//         console.log("prom: ", nameResult);
+//         resolve(nameResult);
+//       })
+//       .catch(err => console.log("error: ", err));
+//   });
+// };
+
+// ...call fn
+// x();
+
+/*
+==========
+The above is the same as the following
+==========
+*/
+
+let answer;
+var x = async () => {
+  const nameResult = await promptly.prompt("Name");
+  answer = nameResult;
+  // console.log(nameResult);
+};
+
+x();
+console.log("bottom: ", answer);
